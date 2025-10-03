c695eb2ecb6c18be474069cb54ae7ffc668b66a5
askaltzy <askaltzy79@gmail.com>
2025-10-03 21:25:43 +0800

    Add files via upload

---
+++ css/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  tokens: "TOKEN BOTLU",  // Ubah Jadi Token Bot Mu !!!
+  owner: "6304082972", // Ubah Jadi Id Mu !!!
+  port: "5000", // Ubah Jadi Port Panel Mu !!!
+  ipvps: "0.0.0.0" // Ubah Jadi Ip Vps Mu !!!
+};
