3cc4c08eac39c1015e69bb5887b121f9a5e6755e
askaltzy <askaltzy79@gmail.com>
2025-10-03 21:46:09 +0800

    Delete css/config.js

--- css/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  tokens: "TOKEN BOTLU",  // Ubah Jadi Token Bot Mu !!!
-  owner: "6304082972", // Ubah Jadi Id Mu !!!
-  port: "5000", // Ubah Jadi Port Panel Mu !!!
-  ipvps: "0.0.0.0" // Ubah Jadi Ip Vps Mu !!!
-};
